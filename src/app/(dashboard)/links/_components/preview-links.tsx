be3387ff76a2a2716cd97117f127d3fa73b742e2
adzibilal <adzibilal02@gmail.com>
2024-02-21 11:42:13 +0700

    feat: preview links

--- src/app/(dashboard)/links/_components/preview-links.tsx
+++ src/app/(dashboard)/links/_components/preview-links.tsx
@@ -6,13 +6,13 @@ import Image from 'next/image'
 const PreviewLinks = ({ links }: { links: Links[] }) => {
     return (
         <div className='dark:bg-zinc-800 bg-muted rounded-3xl overflow-hidden max-w-md mx-auto p-5 min-h-[80vh] pt-16 relative'>
-            <div className="dark:bg-zinc-950 bg-white w-[50%] h-10 absolute top-0 rounded-b-lg left-[50%] translate-x-[-50%]"></div>
+            <div className='dark:bg-zinc-950 bg-white w-[50%] h-10 absolute top-0 rounded-b-lg left-[50%] translate-x-[-50%]'></div>
             {links.map((link, index) => (
                 <div
                     key={index}
                     className={`dark:bg-zinc-700 bg-zinc-200 text-center p-3 mb-2 rounded-md ${
                         link.important ? 'animate-bounce mt-6' : ''
-                    }`}>
+                    } ${link.active ? '' : 'hidden'}`}>
                     <Link
                         href={link.link!}
                         target='_blank'
@@ -24,7 +24,9 @@ const PreviewLinks = ({ links }: { links: Links[] }) => {
                             height={50}
                             className='rounded-md aspect-square overflow-hidden object-cover'
                         />
-                        <p className='dark:text-white text-gray-500'>{link.title}</p>
+                        <p className='dark:text-white text-gray-500'>
+                            {link.title}
+                        </p>
                     </Link>
                 </div>
             ))}
